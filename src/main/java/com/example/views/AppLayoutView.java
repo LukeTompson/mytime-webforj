d81b63108be2aef2bbf94e2a66b58c02dfb692e4
LukeTompson <ltompson@basis.cloud>
2024-11-25 11:20:01 -0700

    Added form with partially-completed layout

--- src/main/java/com/example/views/AppLayoutView.java
+++ src/main/java/com/example/views/AppLayoutView.java
@@ -1,19 +1,30 @@
 package com.example.views;
 
+import java.util.List;
+
 import com.webforj.annotation.InlineStyleSheet;
 import com.webforj.component.Composite;
 import com.webforj.component.Theme;
 import com.webforj.component.button.Button;
+import com.webforj.component.button.ButtonTheme;
 import com.webforj.component.element.Element;
 import com.webforj.component.html.elements.Div;
 import com.webforj.component.html.elements.H1;
 import com.webforj.component.html.elements.H3;
 import com.webforj.component.html.elements.Img;
 import com.webforj.component.html.elements.Paragraph;
+import com.webforj.component.field.TextField;
+import com.webforj.component.field.PasswordField;
+import com.webforj.component.field.TextArea;
 import com.webforj.component.icons.Icon;
 import com.webforj.component.icons.TablerIcon;
 import com.webforj.component.layout.applayout.AppDrawerToggle;
 import com.webforj.component.layout.applayout.AppLayout;
+import com.webforj.component.layout.columnslayout.ColumnsLayout;
+import com.webforj.component.layout.flexlayout.FlexDirection;
+import com.webforj.component.layout.flexlayout.FlexLayout;
+import com.webforj.component.list.ChoiceBox;
+import com.webforj.component.optioninput.CheckBox;
 import com.webforj.component.tabbedpane.Tab;
 import com.webforj.component.tabbedpane.TabbedPane;
 import com.webforj.component.tabbedpane.TabbedPane.Placement;
@@ -33,9 +44,10 @@ public class AppLayoutView extends Composite<AppLayout> {
 	Div header = new Div();
 	Div drawer = new Div();
   Div dashboardContent = new Div();
-  Div newEntryContent;
+  FlexLayout newEntryContent = new FlexLayout(FlexDirection.COLUMN);
   Div editEntryContent = new Div();
   Div customersContent = new Div();
+  ColumnsLayout newEntryForm;
 
 	public AppLayoutView() {
 
@@ -80,15 +92,48 @@ public class AppLayoutView extends Composite<AppLayout> {
 	}
 
 	private void constructDashboard(){
-    Button btn2 = new Button("Get age");
+    Button btn2 = new Button("Dashboard");
     demo.addToContent(dashboardContent);
     dashboardContent.add(btn2);
   }
 
   private void constructNewEntry() {
-    Button btn3 = new Button("New entry");
-    newEntryContent = new Div(btn3);
-    demo.addToContent(newEntryContent);
+    TextArea description = new TextArea("Description");
+    TextArea internalNotes = new TextArea("Internal notes");
+    ChoiceBox customerSelect = new ChoiceBox("Customer");
+    CheckBox billablecCheckBox = new CheckBox("Billable");
+    CheckBox discountedCheckBox = new CheckBox("Discounted");
+    TextField hoursField = new TextField("Hours");
+    Button addEntryBtn = new Button("Add Entry", ButtonTheme.PRIMARY);
+    Button deleteDraftBtn = new Button("Delete Draft", ButtonTheme.DANGER);
+
+    newEntryForm = new ColumnsLayout(
+      List.of(
+          new ColumnsLayout.Breakpoint("default", 0, 1),
+          new ColumnsLayout.Breakpoint("small", "20em", 1),
+          new ColumnsLayout.Breakpoint("medium", "40em", 2),
+          new ColumnsLayout.Breakpoint("large", "60em", 4)),
+          description,customerSelect, billablecCheckBox,
+          discountedCheckBox, hoursField,internalNotes,addEntryBtn,deleteDraftBtn);
+    
+    // newEntryForm.setColumn(description, "default", 4);
+
+    newEntryForm.setSpan(description, 4);
+    newEntryForm.setSpan(customerSelect, 1);
+    newEntryForm.setSpan(billablecCheckBox, 1);
+    newEntryForm.setSpan(discountedCheckBox, 1);
+    newEntryForm.setSpan(hoursField, 1);
+    newEntryForm.setSpan(internalNotes, 4);
+    newEntryForm.setColumn(addEntryBtn, 2);
+    newEntryForm.setColumn(deleteDraftBtn, 3);
+    
+
+    newEntryForm.addClassName("description");
+
+    // Button btn3 = new Button("New entry");
+    newEntryContent.addClassName("formContent").add(newEntryForm);
+    // demo.addToContent(newEntryContent);
+    demo.add(newEntryForm);
   }
 
   private void constructEditEntry() {
